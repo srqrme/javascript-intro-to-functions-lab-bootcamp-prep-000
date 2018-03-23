fb025f01237971c900881c45c0e2a097e215864f
Sharmaine Requerme <srqrme@gmail.com>
2018-03-23 17:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    console.log("I can't hear you!")
+    return `I can't hear you!`
   }
 }
 
