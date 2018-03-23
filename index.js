fb628222eaaf023544912771666dcfe4d800391a
Sharmaine Requerme <srqrme@gmail.com>
2018-03-23 17:33:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 
-  if (string === string.toUpperCase()) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
 }
